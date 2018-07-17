218588427f79cd7a796ea1edf87a3599627dac6a
Aakash Sarang <sarangaakash@gmail.com>
2018-07-17 13:18:15 -0700

    Finish writing APIs

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//api route
+var api = require('./routes/api.route');
+
 var app = express();
 
 //including bluebird library
@@ -37,7 +40,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
+//use api route
+app.use('./api', api);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
