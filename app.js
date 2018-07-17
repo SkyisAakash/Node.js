e9fc182f0055d8c9b29c0df45c6282f455cf8fd0
Aakash Sarang <sarangaakash@gmail.com>
2018-07-17 12:24:17 -0700

    Add todo model

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//including bluebird library
+var bluebird = require('bluebird')
+
+//Adding mongoose and mongodb
+var mongoose = require('mongoose')
+mongoose.Promise = bluebird
+mongoose.connect('mongodb://127.0.0.1:27017/todoapp', {useMongoClient: true})
+.then(() => {console.log('connected to mongodb database')})
+.catch(() => {console.log("Error connecting mongodb")})
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
